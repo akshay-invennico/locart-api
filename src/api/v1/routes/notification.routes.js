9f6ba00a6014069e3740029406e229c2b1412c2e
Akshay Vishwakarma <akshay.vishwakarma@invennico.com>
2025-11-26 13:53:18 +0530

    fix: route

--- src/api/v1/routes/notification.routes.js
+++ src/api/v1/routes/notification.routes.js
@@ -12,7 +12,7 @@ const authMiddleware = require("../../../middlewares/auth.middleware");
 
 router.get("/", authMiddleware(["merchant", "customer", "loctitian"]), getNotifications);
 router.patch("/read/:id", authMiddleware(["merchant", "customer", "loctitian"]), markAsRead);
-router.patch("/read-all", authMiddleware(["merchant", "customer", "loctitian"]), markAllAsRead);
+router.patch("/read", authMiddleware(["merchant", "customer", "loctitian"]), markAllAsRead);
 router.delete("/:id", authMiddleware(["merchant", "customer", "loctitian"]), deleteNotification);
 router.delete("/", authMiddleware(["merchant", "customer", "loctitian"]), clearAllNotifications);
 router.patch("/mute", authMiddleware(["merchant", "customer", "loctitian"]), toggleMuteNotifications);
